32c4d6c8dbe86836ef8bee5310b21fc0a4d178ce
Nom <nom@Noms-iMac.local>
2025-02-04 05:37:59 -0800

    Created the Challenge Controller

---
+++ custom/ChallengeController.js
@@ -0,0 +1,43 @@
+class ChallengeController {
+	constructor() {
+		if (ChallengeController.instance) {
+			return ChallengeController.instance;
+		}
+
+		this.challenges = [];
+		ChallengeController.instance = this;
+	}
+
+	createChallenge(p1, p2) {
+		const uid = createId(p1, p2);
+		if (this.challenges.findIndex((e) => e === uid) !== -1) {
+			return false;
+		}
+
+		console.log("Challenge Created");
+		this.challenges.push(uid);
+		//const challenge = new Challenge(uid);
+		//this.challenges.push(challenge);
+
+		return true;
+	}
+
+	deleteChallenge(uid) {
+		const idx = this.challenges.findIndex((e) => e === uid);
+		if (idx !== -1) {
+			return false;
+		}
+
+		console.log("Challenge Deleted");
+		this.challenges.splice(idx, 1);
+
+		return true;
+	}
+
+	createId(p1, p2) {
+		const ids = [p1, p2].sort();
+		const uid = `${ids[0]}-${ids[1]}`;
+		return uid;
+	}
+
+}
