ce78e584e9f58e9102274a79aa7ee05274b15234
Nom <nom@Noms-iMac.local>
2025-02-04 22:10:03 -0800

    something im sure

--- commands/utility/challenge.js
+++ commands/utility/challenge.js
@@ -13,15 +13,16 @@ module.exports = {
 		.addNumberOption(option =>
 			option
 				.setName('time')
-				.setDescription('The amount of time to wager')
+				.setDescription('The amount of time to wager (in minutes)')
 				.setRequired(false)
 		),
 	async execute(interaction) {
 		console.log(interaction);
 		const p1 = interaction.user;
 		const p2 = interaction.options.getUser('target');
+		console.log(p2);
 		const time = interaction.options.getNumber('time') ?? 5;
-		await interaction.reply(`${p1.username} has challenged ${p2.username}. They have wagered ${time} minutes`);
+		await interaction.reply(`${p1.globalName} has challenged ${p2.globalName}. They have wagered ${time} minutes`);
 
 	},
 };
